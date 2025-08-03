de4003dd20f4b69801fe87c5cac9bc54afbd7f4e
Abhi-alt <chocolate.ladu@gmail.com>
2025-08-03 20:12:34 +0530

    overview

--- src/app/components/overiew/overview.component.ts
+++ src/app/components/overiew/overview.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-overiew',
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
-  imports: [MatDividerModule, MatCardModule],
+  imports: [MatDividerModule, MatCardModule, CurrencyPipe, MatButtonModule],
 })
 export class OverviewComponent {}
